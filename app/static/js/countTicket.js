a6faf0bb914576a94a6d2ae5c09a484c80dbbf7a
Nhất Duy <nhatduy242@gmail.com>
2024-12-02 20:43:37 +0700

    ss

--- app/static/js/countTicket.js
+++ app/static/js/countTicket.js
@@ -2,4 +2,4 @@ document.addEventListener('DOMContentLoaded', function(){
       const ticket = document.querySelectorAll(".ticket");
       const countTicket = document.getElementById("count-ticket")
       countTicket.textContent = ticket.length;
-})
+})    
